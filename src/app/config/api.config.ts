a399f550dc1d547f8d02bf5ab268327b9944a870
Codexa <91712353+CodexaCP@users.noreply.github.com>
2026-07-18 08:48:35 -0300

    fixes to work with tunnel

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,4 @@
-export let API_BASE_URL = 'http://2.25.187.20:5071/api';
+export let API_BASE_URL = 'https://preston-brave-alerts-colon.trycloudflare.com/api';
 
 export function initApiConfig(cfg: { apiUrl: string }): void {
     API_BASE_URL = cfg.apiUrl;
